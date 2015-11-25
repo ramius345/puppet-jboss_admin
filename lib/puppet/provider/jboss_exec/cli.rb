f8257e143e6d6c15c6c097b0181024a9e5465524
Thomas Peters-Hall <tpetersh@redhat.com>
2015-11-25 13:07:39 -0600

    testing out a theory

--- lib/puppet/provider/jboss_exec/cli.rb
+++ lib/puppet/provider/jboss_exec/cli.rb
@@ -8,12 +8,37 @@ Puppet::Type.type(:jboss_exec).provide(:cli) do
   def execute_command command, arguments = nil
     # don't try to execute a command if noop
     unless resource[:noop]
-      if arguments
-        parser = CliParser.new
-        parsed_command = parser.parse_command command
-        command = format_command PathGenerator.format_path(parsed_command[0]), parsed_command[1], arguments 
+      tries = 3
+      try_sleep = 3
+      last_error = nil
+      result = nil
+
+      tries.times do |try|
+        debug("Execute command try #{try+1}/#{tries}") if try > 1
+        begin
+          if arguments
+            parser = CliParser.new
+            parsed_command = parser.parse_command command
+            command = format_command PathGenerator.format_path(parsed_command[0]), parsed_command[1], arguments 
+          end
+
+          if try > 1
+            debug("Sleeping for #{try_sleep} seconds")
+            sleep try_sleep
+          end
+
+          result = execute_cli get_server(resource), command, false
+          raise "Execution failed in execute_command" unless result['outcome'] == 'success'
+          return result
+        rescue => e
+          last_error = e
+        end
       end
-      execute_cli get_server(resource), command, false
+
+      if last_error
+        raise last_error
+      end
+      result
     end
   end
 end
